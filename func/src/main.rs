fba9957ab0f73c7d02d0641ad01ab5f3a90b9bc4
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2023-04-01 14:21:50 +0530

    modified

--- func/src/main.rs
+++ func/src/main.rs
@@ -9,5 +9,6 @@ fn func(s1: &String)->usize{
    s1.len()
    //we have to return length
 }
-
-
+//references are immutable  by default
+// we can't modify something which is passed by reference
+//by adding mut keyword we can allow mutability to references as well
